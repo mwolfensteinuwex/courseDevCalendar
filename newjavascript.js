bdd67843d318a21ed7c9d7e6461e6eb8e35d0e26
Moses Wolfenstein <moses.wolfenstein@uwex.edu>
2015-02-17 11:06:14 -0600

    Lots of changes
    Broken version of array based sorting for spring, lots of content
    changes in the html.

--- newjavascript.js
+++ newjavascript.js
@@ -3,41 +3,38 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-var sequence = ["#jan", "#feb", "#mar", "#apr", "#may", "#jun", "#jul", "#aug", "#sep", "#oct", "#nov", "#dec"];
+
 
 $(document).ready(function(){
     $("span.fall").hide();
     $("span.spring").hide();
     $("span.summer").hide();
-});
 
-$(document).ready(function(){
     $("#springSelect").click(function(){
         $(".spring").show();
         $(".fall").hide();
         $(".summer").hide();
-        
+        var $sequence = $("#year .months");
+        var springOrder = [4, 5, 6, 7, 8, 9, 10, 11, 0, 1, 2, 3];
+        var springSequence = document.getElementById("year");
+        for (var i = 0, l = springOrder.length; i < l; i++) {
+            springSequence.appendChild($sequence.get(springOrder[i]));
+        };
 
     });
-});
 
-$(document).ready(function(){
     $("#fallSelect").click(function(){
         $(".fall").show();
         $(".spring").hide();
         $(".summer").hide();
     });
-});
 
-$(document).ready(function(){
     $("#summerSelect").click(function(){
         $(".summer").show();
         $(".fall").hide();
         $(".spring").hide();
     });
-});
 
-$(document).ready(function(){
     $("#allSelect").click(function(){
         $(".summer").show();
         $(".fall").show();
@@ -45,5 +42,6 @@ $(document).ready(function(){
         $("span.summer").hide();
         $("span.fall").hide();
         $("span.spring").hide();
+        
     });
 });
